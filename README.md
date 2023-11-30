94a0c3a9ef9cb47779a1537eb805ca846f37f848
sanjaya.koju <sanjayakoju42@gmail.com>
2023-11-30 14:35:01 -0800

    kafka Demo

---
+++ README.md
@@ -0,0 +1,5 @@
+### To run the project download and run kafka
+
+```https://kafka.apache.org/downloads```
+
+```https://kafka.apache.org/quickstart```
---
+++ kafka-consumer-wikimedia/src/main/java/com/consumer/kafka/KafkaWikimediaConsumer.java
@@ -0,0 +1,22 @@
+package com.consumer.kafka;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaWikimediaConsumer {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(KafkaWikimediaConsumer.class);
+
+
+    @KafkaListener(
+            topics = "${spring.kafka.topic.name}",
+            groupId = "${spring.kafka.consumer.group-id}"
+    )
+    public void consume(String eventMessage){
+
+        LOGGER.info(String.format("Event message received -> %s", eventMessage));
+    }
+}
